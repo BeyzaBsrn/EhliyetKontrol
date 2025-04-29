4ec80118750a772160ea0d88ff4c3102ab3cf0e8
BeyzaBsrn <beyzabasaran03@gmail.com>
2025-04-29 07:54:54 +0300

    Add project files.

---
+++ Odev.6/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Odev._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen yaşınızı giriniz: ");
+            int yas = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("lütfen sağlık durumunuzu giriniz(evet/hayır): ");
+            string saglikDurumu = Console.ReadLine().Trim().ToLower();
+            Console.WriteLine("Lütfen sabıkanız var mı giriniz (evet/hayır): ");
+            string sabika = Console.ReadLine().Trim().ToLower();
+
+            Console.WriteLine("girdiğiniz bilgiler şunlardır:");
+            Console.WriteLine($"YAŞ: {yas}");
+            Console.WriteLine($"SAĞLIK DURUMU:{saglikDurumu}");
+            Console.WriteLine($"SABIKA DURUMU: {sabika}");
+
+            if(yas >= 18 && saglikDurumu == "evet" && sabika == "hayır")
+            {
+                Console.WriteLine("ehliyet almanız uygundur.");
+            }
+            else
+            {
+                Console.WriteLine("ehliyet alamazsınız");
+            }
+
+
+
+        }
+    }
+}
